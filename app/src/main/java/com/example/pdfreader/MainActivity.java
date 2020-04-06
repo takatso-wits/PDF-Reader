c9164b7d9af59a35eccf9001421e76a25d59f095
takatso-wits <569869@students.wits.ac.za>
2020-04-06 21:29:40 +0200

    Everything works fine.

--- app/src/main/java/com/example/pdfreader/MainActivity.java
+++ app/src/main/java/com/example/pdfreader/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("FinalDocument.pdf")
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .spacing(0)
+                .load();
     }
+
+
 }
